482f13e24e32d6223ee370c159bde3e00b93ac0a
陈浩 <ch_improve@163.com>
2020-01-16 14:22:08 +0800

    22

--- myppt/containts.py
+++ myppt/containts.py
@@ -1,4 +1,4 @@
-QN_ACCESS = 'hDdbWNnQtqv_8km-l0QMyo6drJGpxbix5_2GahxU'
-QN_SECRET = 'JG7evJQapeoHVSiZcFkJQBEb6LPBdr3zRU7HXiJV'
-QN_URL = 'http://q3bj0ygg8.bkt.clouddn.com'
-QN_BUCKET = 'qmx'
+QN_ACCESS = 'd-GgH0KIIEai9N_Ax1VbNyD4uqpqjOHvmvjsYW8f'
+QN_SECRET = 'ELIi7_ncY0NqtDghAnc7bPQPcrYEhAHXpe9gqqqf'
+QN_URL = 'qny-qmxlyw-com-idva350.qiniudns.com'
+QN_BUCKET = 'qmxzyx'
